51866b5dbb527c5efe999accf401b87721a184fa
muhammadahmadd <66084909+muhammadahmadd@users.noreply.github.com>
2022-04-13 11:21:24 +0500

    adding

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Fun from './components/fun';
 import Clas from './components/clas';
+const port =process.env.PORT ||3000;
 
 function App() {
   return (
     <div className="App">
-      <Fun name="ahmad" age="24">
+      <Fun name="ahmad" age="24">s
         <p1>hello</p1>
       </Fun>
       <Clas></Clas>
